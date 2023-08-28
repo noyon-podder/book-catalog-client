0de76df99a2a6f747e9db05f7117b6174be5862d
noyon-podder <noyon.vai369@gmail.com>
2023-08-28 19:09:18 +0600

    banner section add this commit

---
+++ src/pages/home/Banner/Banner.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner-image flex items-center px-20 justify-center">
+      <div className="container mx-auto py-10 lg:px-20 px-5 flex items-center justify-center">
+        <div className="text-center w-full">
+          <h3 className="uppercase text-white text-2xl lg:text-5xl font-extrabold text-center mb-4 lg:mb-8">
+            Book Reading
+          </h3>
+          <h6 className="flex items-center gap-x-2 text-white justify-center text-2xl capitalize font-semibold">
+            <span className="w-10 h-1 bg-white inline-block"></span> best book
+            available
+            <span className="w-10 h-1 bg-white inline-block"></span>
+          </h6>
+          <p className="text-gray-200 text-lg w-3/4 mx-auto mt-2 hidden lg:block">
+            Authoritatively recaptiualize dynamic technology after ubiquitous
+            metrics. Rapidiously promote dynamic materials rather.
+          </p>
+          <div className="flex items-center justify-center mt-6">
+            <div className=" w-full md:w-2/4 flex search-field">
+              <input
+                type="text"
+                className="w-full py-2  px-4 outline-none "
+                placeholder="Search your favorite book"
+              />
+              <Link className=" px-4 py-2 text-white bg-pink-500 text-lg font-semibold">
+                Search
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="lg:mt-10 mt-5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
