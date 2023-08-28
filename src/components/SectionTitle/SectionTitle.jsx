db308a8ccab75a034f9d66fbbece5d56b30e4d93
noyon-podder <noyon.vai369@gmail.com>
2023-08-29 02:18:55 +0600

    our team add this commit

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import "./SectionTitle.css";
 
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className="w-full text-center mb-5 lg:mb-10">
+    <div className="w-full text-center mb-5 ">
       <h2 className="top-heading">{heading}</h2>
       <p className="sub-heading">{subHeading}</p>
       <div className="code-icon flex items-center justify-center ">
--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -1,12 +1,13 @@
 import Banner from "../Banner/Banner";
 import LatestBooks from "../LatestBooks/LatestBooks";
+import TopAuthors from "../TopAuthors/TopAuthors";
 
 const Home = () => {
   return (
     <>
       <Banner />
-
       <LatestBooks />
+      <TopAuthors />
     </>
   );
 };
